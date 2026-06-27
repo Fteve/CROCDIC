d7bb81c46a9846f9cc81fdfff0989bb026557001
Steven Cheng <chengersteve@gmail.com>
2026-06-27 11:14:12 +0200

    Trying to figure out postlayout sim

---
+++ init_tech.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p technology/verilog
+
+ln -s /usr/pack/ihp-sg13-kgf/open_ihp_sg13g2/sg13g2_sram/v2.1/verilog/* ./technology/verilog/
+ln -s /usr/pack/ihp-sg13-kgf/eth/ez130_8t_stdcell/v1.0/verilog/* ./technology/verilog/
+ln -s /usr/pack/ihp-sg13-kgf/open_ihp_sg13g2/sg13g2camos5l_io/v0.2/verilog/* ./technology/verilog/
+
--- rtl/test/tb_croc_soc.sv
+++ rtl/test/tb_croc_soc.sv
@@ -87,7 +87,7 @@ module tb_croc_soc #(
   ////////////
   //  DUT   //
   ////////////
-`ifdef TARGET_NETLIST_OPENROAD
+`ifdef TARGET_POSTLAYOUT
     wire  [GpioCount-1:0] gpio_io_tb;
     logic [GpioCount-1:0] gpio_en_tb;
 
